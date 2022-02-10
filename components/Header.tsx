9663f6c8a6645957eb0d61cc5823dcf1d32b7d4f
cuzekn <cuzekn@gmail.com>
2022-02-10 23:56:04 +0900

    chore: TS化

---
+++ components/Header.tsx
@@ -0,0 +1,59 @@
+import Head from "next/head";
+import Link from "next/link";
+import { VFC } from "react";
+
+type Props = {
+  children: any;
+  title: string;
+}
+
+export const Header: VFC<Props> = ({ children, title = "HP by Nextjs" }) => {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-green-900 text-sm font-mono">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <header>
+        <nav className="bg-green-800 w-screen">
+          <div className="flex items-center pl-8 h-14">
+            <div className="flex space-x-4">
+              <Link href="/">
+                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                  HOME
+                </a>
+              </Link>
+              <Link href="/about">
+                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                  About
+                </a>
+              </Link>
+              <Link href="/profile">
+                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                  Profile
+                </a>
+              </Link>
+              <Link href="/album">
+                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                  Album
+                </a>
+              </Link>
+              <Link href="/login">
+                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                  Login
+                </a>
+              </Link>
+              <Link href="/signup">
+                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                  Signup
+                </a>
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main className="flex flex-1 justify-center items-center flex-col w-screen">
+        {children}
+      </main>
+    </div>
+  );
+}
