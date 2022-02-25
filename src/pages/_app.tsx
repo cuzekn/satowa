b1fa0f383736a1963cb93594157c56c196a7ee82
cuzekn <cuzekn@gmail.com>
2022-02-26 01:23:27 +0900

    docs: cssの表記変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 
-import 'src/styles/globals.css'
+import 'src/styles/styles.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
