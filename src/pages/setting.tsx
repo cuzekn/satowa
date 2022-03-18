a0520c4b2caff6f91143d50b8d0f79d9aee19e51
cuze <cuzekn@gmail.com>
2022-03-18 19:44:10 +0900

    feat: ログアウト機能の実装

--- src/pages/setting.tsx
+++ src/pages/setting.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Header } from "src/components/Layout/Header";
+import { auth } from "src/firebase/firebase";
 
 const setting = () => {
-  return <Header title="設定">setting</Header>;
+  return (
+    <>
+      <Header title="設定">setting</Header>
+      <button onClick={() => auth.signOut()}>Logout</button>
+    </>
+  );
 };
 
 export default setting;
