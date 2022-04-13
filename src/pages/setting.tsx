f525829610f1fe32e5d37d889aa8f3b7c329c9a3
cuze <cuzekn@gmail.com>
2022-04-14 02:00:36 +0900

    feat: 退会機能の実装

--- src/pages/setting.tsx
+++ src/pages/setting.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { signOut } from "firebase/auth";
+import { deleteUser, signOut } from "firebase/auth";
 
 import { Header } from "src/components/Layout/Header";
 import { auth } from "src/firebase/firebaseConfig";
 
 const setting = () => {
+  const user = auth.currentUser;
   return (
     <>
       <Header title="設定">setting</Header>
       <button onClick={() => signOut(auth)}>Logout</button>
+      <br />
+      <button onClick={() =>deleteUser(user!)}>退会</button>
     </>
   );
 };
