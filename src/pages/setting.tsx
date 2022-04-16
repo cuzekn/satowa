235595c88c2a936c6615d4fb7df6716aa11180f6
cuze <cuzekn@gmail.com>
2022-04-16 20:27:30 +0900

    feat: ログアウト、退会後のリダイレクト機能を実装

--- src/pages/setting.tsx
+++ src/pages/setting.tsx
@@ -3,15 +3,18 @@ import { deleteUser, signOut } from "firebase/auth";
 
 import { Header } from "src/components/Layout/Header";
 import { auth } from "src/firebase/firebaseConfig";
+import { useRouter } from "next/router";
 
 const setting = () => {
   const user = auth.currentUser;
+  const router = useRouter();
+
   return (
     <>
       <Header title="設定">setting</Header>
-      <button className="bg-primary-green text-white p-3 rounded-lg mx-20 my-5" onClick={() => signOut(auth)}>Logout</button>
+      <button className="bg-primary-green text-white p-3 rounded-lg mx-20 my-5" onClick={() => {signOut(auth), router.push("/home")}}>Logout</button>
       <br />
-      <button className="bg-primary-green text-white p-3 rounded-lg mx-20" onClick={() =>deleteUser(user!)}>退会</button>
+      <button className="bg-primary-green text-white p-3 rounded-lg mx-20" onClick={() =>{deleteUser(user!), router.push("/home")}}>退会</button>
     </>
   );
 };
