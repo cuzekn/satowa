30d6c7a1133a6d76e98a17e2b4941344487b26bf
cuze <cuzekn@gmail.com>
2022-05-06 23:46:55 +0900

    feat: 写真の投稿機能を実装

--- src/pages/photo.tsx
+++ src/pages/photo.tsx
@@ -1,26 +1,28 @@
-import { Avatar, Button, Image } from "@mantine/core";
-import { onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
 import {
+  addDoc,
   collection,
-  doc,
   getDocs,
   onSnapshot,
+  orderBy,
   query,
+  serverTimestamp,
+  where,
 } from "firebase/firestore";
-import React, { useEffect, useState } from "react";
+import { Avatar, Button, Divider, Image } from "@mantine/core";
+import { GiHollowCat } from "react-icons/gi";
+
 import { Header } from "src/components/Layout/Header";
-import { auth, db } from "src/firebase/firebaseConfig";
+import { auth, db, storage } from "src/firebase/firebaseConfig";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import Link from "next/link";
 
 const photo = () => {
   const [comment, setComment] = useState("");
   const [petName, setPetName] = useState("");
-  const [petPhoto, setPetPhoto] = useState([
-    {
-      id: "",
-      petName: "",
-      petImage: "",
-    },
-  ]);
+  const [petImage, setPetImage] = useState<File | null>(null);
+  const [petImagePreview, setPetImagePreview] = useState<null | string>(null);
+  const [petPhoto, setPetPhoto] = useState<File | null>(null);
   const [petAll, setPetAll] = useState([
     {
       id: "",
@@ -43,13 +45,71 @@ const photo = () => {
     console.log(petAll);
   };
 
-  const onChangePetname = (e) => {
+  const onChangePetname = async (e: any) => {
     console.log(e.target.value);
+    setPetName(e.target.value);
+    const name = e.target.value;
+    const q = query(collection(db, "petProfile"), where("petName", "==", name));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data().petImage);
+      setPetImage(doc.data().petImage);
+    });
+  };
+
+  const onChangeImageHandler = async (e: any) => {
+    e.preventDefault();
+    console.log(e.target.files[0]);
+    setPetPhoto(e.target.files[0]);
+    setPetImagePreview(window.URL.createObjectURL(e.target.files[0]));
+  };
+
+  const sendTweet = async (e: any) => {
+    e.preventDefault();
+    let url = "";
+    if (petPhoto) {
+      const S =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      const N = 16;
+      const randomChar = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+        .map((n) => S[n % S.length])
+        .join("");
+      const fileName = randomChar + "_" + petPhoto.name;
+      await uploadBytes(ref(storage, `tweetImages/${fileName}`), petPhoto);
+      url = await getDownloadURL(ref(storage, `tweetImages/${fileName}`));
+    }
+    console.log(url);
+
+    await addDoc(collection(db, "tweets"), {
+      petName: petName,
+      petImage: petImage,
+      petPhoto: url,
+      comment: comment,
+      timestamp: serverTimestamp(),
+    });
+
+    setPetName("");
+    setPetImage(null);
+    setPetImagePreview(null);
   };
 
   useEffect(() => {
-    const q = query(collection(db, "petProfile"));
+    const q = query(collection(db, "tweets"));
     onSnapshot(q, (snapshot) => {
+      setTweets(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          petName: doc.data().petName,
+          petImage: doc.data().petImage,
+          comment: doc.data().comment,
+          petPhoto: doc.data().petPhoto,
+          timestamp: doc.data().timestamp,
+        }))
+      );
+    });
+
+    const profile = query(collection(db, "petProfile"));
+    onSnapshot(profile, (snapshot) => {
       setPetAll(
         snapshot.docs.map((doc) => ({
           id: doc.id,
@@ -60,18 +120,47 @@ const photo = () => {
     });
   }, []);
 
+  const [tweets, setTweets] = useState([
+    {
+      id: "",
+      petName: "",
+      petImage: "",
+      comment: "",
+      petPhoto: "",
+      timestamp: null,
+    },
+  ]);
+  const getTweets = () => {
+    const q = query(collection(db, "tweets"));
+    onSnapshot(q, (snapshot) => {
+      setTweets(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          petName: doc.data().petName,
+          petImage: doc.data().petImage,
+          comment: doc.data().comment,
+          petPhoto: doc.data().petPhoto,
+          timestamp: doc.data().timestamp,
+        }))
+      );
+    });
+  };
+
   return (
     <Header title="写真一覧">
       <button onClick={getPet} className="bg-gray-500 text-white p-2">
         GetPet
       </button>
-      <form className="bg-primary-green w-96 m-10 px-8 py-2">
+      <form
+        onSubmit={sendTweet}
+        className="bg-primary-green w-96 m-10 px-8 py-2"
+      >
         <div className="flex">
           <div>
-            <Avatar src={petPhoto} className="rounded-full w-12 h-12" />
+            <Avatar src={petImage} className="rounded-full w-12 h-12" />
           </div>
           <div className="mx-1">
-            <select name="" id="" onChange={onChangePetname}>
+            <select onChange={onChangePetname}>
               {petAll.map((pet) => (
                 <option key={pet.id} value={pet.petName}>
                   {pet.petName}
@@ -81,12 +170,30 @@ const photo = () => {
             <input
               className="text-primary-brown bg-white w-56 rounded-md p-1"
               type="text"
-              placeholder="コメント"
+              placeholder="タイトル"
               onChange={(e) => setComment(e.target.value)}
             />
           </div>
         </div>
-        <div className="bg-gray-300 w-80 h-48 mx-auto my-3">img</div>
+        <label htmlFor="image">
+          <div className="bg-gray-300 w-80 h-48 mx-auto my-3 cursor-pointer">
+            {petImagePreview ? (
+              <img
+                src={petImagePreview}
+                className="w-full h-full object-cover"
+              />
+            ) : (
+              <GiHollowCat size={64} className="m-auto h-32 text-center" />
+            )}
+          </div>
+          <input
+            id="image"
+            type="file"
+            className="hidden"
+            accept=".jpg, .jpeg, .png"
+            onChange={onChangeImageHandler}
+          />
+        </label>
         <button
           type="submit"
           disabled={!comment}
@@ -95,6 +202,47 @@ const photo = () => {
           作成
         </button>
       </form>
+
+      <Divider size="md" />
+
+      <button onClick={getTweets} className="bg-gray-500 text-white p-2">
+        getTweets
+      </button>
+      <div className="flex">
+        {tweets.map((pet) => (
+          <div key={pet.id} className="bg-primary-green w-96 m-10">
+            <div className="flex">
+              <div>
+                <img
+                  src={pet.petImage}
+                  height={60}
+                  width={60}
+                  className="m-3 rounded-full"
+                />
+              </div>
+              <div className="mx-1">
+                <p className="text-white p-1 mt-2 mb-1">{pet.petName}</p>
+                <p className="text-primary-brown bg-white w-64 rounded-md m-0 p-1">
+                  {pet.comment}
+                </p>
+              </div>
+              {/* <p>{pet.timestamp}</p> */}
+            </div>
+            <img
+              src={pet.petPhoto}
+              className="block object-cover h-48 mx-auto my-3"
+            />
+            <div className="flex">
+              <Link href="/">
+                <a className="block mx-auto text-center text-primary-orange text-lg m-4 flex-1">
+                  Tap
+                </a>
+              </Link>
+              <input type="checkbox" />
+            </div>
+          </div>
+        ))}
+      </div>
     </Header>
   );
 };
