26d42ec9d90743bffc54f67ca863a67f6cfb76aa
cuze <cuzekn@gmail.com>
2022-05-20 23:20:31 +0900

    feat: 新規登録時にユーザー情報をFirestoreに保存 #11

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { onAuthStateChanged, signInWithPopup } from "firebase/auth";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import { collection, addDoc, query, getDocs } from "firebase/firestore";
-
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithPopup,
+  updateProfile,
+} from "firebase/auth";
+import { Divider, Notification } from "@mantine/core";
+import { FiCheckCircle } from "react-icons/fi";
 
 import { Header } from "src/components/Layout/Header";
-import { auth, db, provider, singupUserWithEmailAndPassword } from "src/firebase/firebaseConfig";
-import Link from "next/link";
+import { auth, db, provider } from "src/firebase/firebaseConfig";
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
 
 const Signup: React.FC = () => {
   const [username, setUsername] = useState("");
@@ -15,17 +21,45 @@ const Signup: React.FC = () => {
   const router = useRouter();
 
   const signInGoogle = async () => {
-    await signInWithPopup(auth, provider).catch((err) => alert(err.message));
+    await signInWithPopup(auth, provider)
+      .then(
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+            const uid = user.uid;
+            console.log(uid);
+          } else {
+            console.log("user is null");
+          }
+        })
+      )
+      .catch((err) => alert(err.message));
   };
 
   const signUp = async (e: any) => {
     e.preventDefault();
-    const user = await singupUserWithEmailAndPassword(email, password);
-    await addDoc(collection(db, "posts"), {
-      userName: username,
-    })
-    console.log(username);
-    
+    try {
+      const authUser = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (authUser.user) {
+        await updateProfile(authUser.user, {
+          displayName: username,
+          photoURL: "",
+        });
+        await setDoc(doc(db, `users/${authUser.user.uid}`), {
+          uid: authUser.user.uid,
+          username: username,
+          email: email,
+          photoURL: "",
+        })
+        console.log(authUser.user);
+      }
+    } catch (error) {
+      alert("登録失敗");
+      console.log(error);
+    }
   };
 
   useEffect(() => {
@@ -39,7 +73,7 @@ const Signup: React.FC = () => {
   return (
     <>
       <Header title="Signup">
-        <div className="flex fixed top-0 right-0 justify-center items-center p-3 m-8 w-6 h-6 font-mono text-xs text-white bg-gray-700 rounded-full sm:bg-pink-500 md:bg-praitext-primary-orange lg:bg-green-500 xl:bg-blue-500">
+        <div className="md:bg-praitext-primary-orange fixed top-0 right-0 m-8 flex h-6 w-6 items-center justify-center rounded-full bg-gray-700 p-3 font-mono text-xs text-white sm:bg-pink-500 lg:bg-green-500 xl:bg-blue-500">
           <div className="block  sm:hidden md:hidden lg:hidden xl:hidden">
             al
           </div>
@@ -57,63 +91,63 @@ const Signup: React.FC = () => {
           </div>
         </div>
 
-        <div className="lg:max-w-5xl mx-auto lg:flex my-28 lg:my-28 drop-shadow-xl ">
-          <div className="mx-auto max-w-sm lg:bg-primary-green rounded-l-3xl sm:max-w-md md:max-w-lg lg:w-1/2 ">
+        <div className="mx-auto my-28 drop-shadow-xl lg:my-28 lg:flex lg:max-w-5xl ">
+          <div className="mx-auto max-w-sm rounded-l-3xl sm:max-w-md md:max-w-lg lg:w-1/2 lg:bg-primary-green ">
             <div className="lg:my-64">
-              <p className="text-primary-orange text-4xl font-bold hidden lg:block text-center ">
+              <p className="hidden text-center text-4xl font-bold text-primary-orange lg:block ">
                 Login
               </p>
               <Link href="/login">
-                <button className="my-7 mx-auto w-96 h-12 text-2xl bg-primary-thinOrange hover:bg-primary-orange hover:text-white rounded-3xl hidden lg:block shadow-lg ">
+                <button className="my-7 mx-auto hidden h-12 w-96 rounded-3xl bg-primary-thinOrange text-2xl shadow-lg hover:bg-primary-orange hover:text-white lg:block ">
                   ログイン
                 </button>
               </Link>
             </div>
           </div>
-          <div className="mx-auto max-w-sm bg-white rounded-r-3xl sm:max-w-md md:max-w-lg lg:w-1/2">
-            <h1 className="pt-7 text-4xl text-center text-primary-orange font-bold">
+          <div className="mx-auto max-w-sm rounded-3xl bg-white sm:max-w-md md:max-w-lg lg:w-1/2 lg:rounded-l-none">
+            <h1 className="pt-7 text-center text-4xl font-bold text-primary-orange">
               SignUp
             </h1>
-            <form className="pt-8" onSubmit={signUp}>
-              <div className="mx-8">
+            <div className="mx-8">
+              <form className="pt-8" onSubmit={signUp}>
                 <p className="my-5 text-2xl">ユーザー名</p>
                 <input
                   type="text"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
-                  className="w-full h-10 rounded-md border-2 border-primary-orange hover:border-primary-thinOrange focus:outline-primary-orange px-2"
+                  className="h-10 w-full rounded-md border-2 border-primary-orange px-2 hover:border-primary-thinOrange focus:outline-primary-orange"
                 />
                 <p className="my-5 text-2xl">メールアドレス</p>
                 <input
                   type="text"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className="w-full h-10 rounded-md border-2 border-primary-orange hover:border-primary-thinOrange focus:outline-primary-brown px-2"
+                  className="h-10 w-full rounded-md border-2 border-primary-orange px-2 hover:border-primary-thinOrange focus:outline-primary-brown"
                 />
                 <p className="my-5 text-2xl">パスワード</p>
                 <input
                   type="password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="w-full h-10 rounded-md border-2 border-primary-orange hover:border-primary-thinOrange focus:outline-primary-brown px-2"
+                  className="h-10 w-full rounded-md border-2 border-primary-orange px-2 hover:border-primary-thinOrange focus:outline-primary-brown"
                 />
                 <button
                   onSubmit={signUp}
-                  className="my-7 w-full h-12 text-2xl text-primary-orange bg-teal-600 hover:bg-primary-green rounded-3xl shadow-lg"
+                  className="my-7 h-12 w-full rounded-3xl bg-teal-600 text-2xl text-primary-orange shadow-lg hover:bg-primary-green"
                 >
                   新規登録
                 </button>
                 <button
-                  className="my-7 w-full h-12 text-2xl bg-primary-thinOrange hover:bg-primary-orange hover:text-white rounded-3xl shadow-lg"
+                  className="my-7 h-12 w-full rounded-3xl bg-primary-thinOrange text-2xl shadow-lg hover:bg-primary-orange hover:text-white"
                   onClick={signInGoogle}
                 >
                   Googleで登録
                 </button>
-                <button className="mb-7 w-full h-12 text-2xl bg-primary-thinOrange hover:bg-primary-orange hover:text-white rounded-3xl shadow-lg">
+                <button className="mb-7 h-12 w-full rounded-3xl bg-primary-thinOrange text-2xl shadow-lg hover:bg-primary-orange hover:text-white">
                   Twitterで登録
                 </button>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </Header>
